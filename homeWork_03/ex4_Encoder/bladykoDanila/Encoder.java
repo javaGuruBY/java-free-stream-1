ff3c3817667ce2aa131993d362a3dff48035a1ea
Dan <54392986+Bell-Kranal@users.noreply.github.com>
2020-02-15 17:51:35 +0300

    task is done, review pls

---
+++ homeWork_03/ex4_Encoder/bladykoDanila/Encoder.java
@@ -0,0 +1,17 @@
+package Encoder;
+
+public class Encoder {
+
+    public Encoder() {
+
+    }
+
+    public void encode(short code) {
+        System.out.println(code + " => '" + ((char)code) + "'");
+    }
+
+    public void decode(char symbol) {
+        System.out.println("'" + symbol + "' => " + ((int)symbol));
+    }
+
+}
---
+++ homeWork_03/ex4_Encoder/bladykoDanila/EncoderDemo.java
@@ -0,0 +1,13 @@
+package Encoder;
+
+public class EncoderDemo {
+    public static void main(String[] args) {
+        Encoder encoder = new Encoder();
+
+        encoder.encode((short)65);
+        encoder.decode('X');
+
+        encoder.encode((short)93);
+        encoder.decode('^');
+    }
+}
