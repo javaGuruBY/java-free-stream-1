33c5e87ad2d2a1d5c117eb59586941f9c62f878a
Vlad Kholad <31291069+nexer1993@users.noreply.github.com>
2020-02-16 20:00:25 +0300

    Add files via upload

---
+++ homeWork_03/ex3_Circle/VladKholad/CircleDemo.java
@@ -0,0 +1,9 @@
+public class CircleDemo {
+    public static void main(String[] args) {
+        Circle firstCircle = new Circle(5);
+        Circle secondCircle = new Circle(36);
+
+        firstCircle.calculateArea();
+        secondCircle.calculateArea();
+    }
+}
