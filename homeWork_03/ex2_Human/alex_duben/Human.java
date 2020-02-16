b8c8da9e03160ecaec0b81551c5a489bc99b999f
sanyafunt <fu_nt.ik@mail.ru>
2020-02-17 01:24:32 +0300

    Human and HumanDemo added

---
+++ homeWork_03/ex2_Human/alex_duben/Human.java
@@ -0,0 +1,15 @@
+public class Human {
+    String name;
+    int age;
+
+    Human(String name, int age) {
+       this.name = name;
+       this.age = age;
+    }
+
+    void greet (){
+        System.out.println("Hi! My name is "+name+", I'm "+age+" years old");
+    }
+
+
+}
---
+++ homeWork_03/ex2_Human/alex_duben/HumanDemo.java
@@ -0,0 +1,7 @@
+public class HumanDemo {
+    public static void main(String[] args) {
+        Human Human1 = new Human("Nikita", 18);
+        Human1.greet();
+    }
+
+}
