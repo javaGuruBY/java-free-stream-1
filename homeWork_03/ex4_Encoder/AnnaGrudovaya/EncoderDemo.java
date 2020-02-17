d618b5ace03b4be3dc56704815b479dee49f6405
Anka330 <61016275+Anka330@users.noreply.github.com>
2020-02-17 16:43:24 +0300

    Add files via upload

---
+++ homeWork_03/ex4_Encoder/AnnaGrudovaya/EncoderDemo.java
@@ -0,0 +1,10 @@
+package Encoder;
+
+public class EncoderDemo {
+    public static void main(String[] args) {
+        Encoder encoder = new Encoder();
+        encoder.encode((short)65);
+        encoder.decode('X');
+
+    }
+}
