51ae6034ad95064caa5edb1c7dc9867aa5a9ded9
Бладыко Данила Евгеньевич <54392986+Bell-Kranal@users.noreply.github.com>
2020-02-19 15:21:50 +0300

    Add files via upload

---
+++ homeWork_04/ex1_SignComparator/Danila_Bladyko/src/jrr/comparator/SignComparator.java
@@ -0,0 +1,27 @@
+package by.jrr.comparator;
+
+public class SignComparator {
+    public String compare(int number) {
+        if (number > 0) {
+            return "Number is positive";
+        }
+        else if (number == 0) {
+            return "Number is equal to zero";
+        }
+        else {
+            return "Number is negative";
+        }
+    }
+
+    public String compare(double number) {
+        if (number > 0) {
+            return "Number is positive";
+        }
+        else if (number == 0) {
+            return "Number is equal to zero";
+        }
+        else {
+            return "Number is negative";
+        }
+    }
+}
