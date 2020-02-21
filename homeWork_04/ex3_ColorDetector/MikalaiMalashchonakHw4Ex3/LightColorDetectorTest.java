5eca844a335f7ef41130344fcd9eca183515c7fc
Mikalai Malashchonak <36278341+malasche@users.noreply.github.com>
2020-02-22 02:23:05 +0300

    LightColorDetectorTest was fixed according to Max comments. Please, Review.

--- homeWork_04/ex3_ColorDetector/MikalaiMalashchonakHw4Ex3/LightColorDetectorTest.java
+++ homeWork_04/ex3_ColorDetector/MikalaiMalashchonakHw4Ex3/LightColorDetectorTest.java
@@ -1,91 +1,71 @@
 package com.javaguru.HomeWork4.hw4ex3;
 
 public class LightColorDetectorTest {
-    public void testColorDetector() {
-        LightColorDetector newLightSource = new LightColorDetector();
+    public void testColorDetectorShouldReturnInvisibleLight() {
+        LightColorDetector newInvisibleLightSource = new LightColorDetector();
         String expectedInvisibleLight =  "Invisible Light";
-        String expectedViolet =  "Violet";
-        String expectedBlue =  "Blue";
-        String expectedGreen = "Green";
-        String expectedYellow = "Yellow";
-        String expectedOrange = "Orange";
-        String expectedRed = "Red";
-        String actualInvisibleLight = newLightSource.detect(325);
-        String actualViolet = newLightSource.detect(385);
-        String actualBlue = newLightSource.detect(453);
-        String actualGreen = newLightSource.detect(500);
-        String actualYellow = newLightSource.detect(580);
-        String actualOrange = newLightSource.detect(600);
-        String actualRed = newLightSource.detect(650);
-        checkInvisible(expectedInvisibleLight, actualInvisibleLight, "testInvisibleLight");
-        checkViolet(expectedViolet, actualViolet, "checkViolet");
-        checkBlue(expectedBlue, actualBlue, "checkBlue");
-        checkGreen(expectedGreen, actualGreen, "checkGreen");
-        checkYellow(expectedYellow, actualYellow, "checkYellow");
-        checkOrange(expectedOrange, actualOrange, "checkOrange");
-        checkRed(expectedRed, actualRed, "checkRed");
+             String actualInvisibleLight = newInvisibleLightSource.detect(325);
+                checkColor(expectedInvisibleLight, actualInvisibleLight, "testColorDetectorShouldReturnInvisibleLight");
+    }
+    public void testColorDetectorShouldReturnViolet() {
+        LightColorDetector newLightSource = new LightColorDetector();
+           String expectedViolet =  "Violet";
+                String actualViolet = newLightSource.detect(385);
+        checkColor(expectedViolet, actualViolet, "testColorDetectorShouldReturnViolet");
 
     }
-    public void checkInvisible(String expectedInvisibleLight, String actualInvisibleLight, String testName) {
-        if (expectedInvisibleLight.equals(actualInvisibleLight)) {
+    public void testColorDetectorShouldReturnBlue() {
+        LightColorDetector newLightSource = new LightColorDetector();
+        String expectedBlue =  "Blue";
+           String actualBlue = newLightSource.detect(453);
+        checkColor(expectedBlue, actualBlue, "testColorDetectorShouldReturnBlue");
+
+    }
+    public void testColorDetectorShouldReturnGreen() {
+        LightColorDetector newLightSource = new LightColorDetector();
+           String expectedGreen = "Green";
+            String actualGreen = newLightSource.detect(500);
+        checkColor(expectedGreen, actualGreen, "testColorDetectorShouldReturnGreen");
+    }
+    public void testColorDetectorShouldReturnYellow() {
+        LightColorDetector newLightSource = new LightColorDetector();
+        String expectedYellow = "Yellow";
+               String actualYellow = newLightSource.detect(580);
+        checkColor(expectedYellow, actualYellow, "testColorDetectorShouldReturnYellow");
+    }
+    public void testColorDetectorShouldReturnOrange() {
+        LightColorDetector newLightSource = new LightColorDetector();
+        String expectedOrange = "Orange";
+            String actualOrange = newLightSource.detect(600);
+        checkColor(expectedOrange, actualOrange, "testColorDetectorShouldReturnOrange");
+
+    }
+    public void testColorDetectorShouldReturnRed() {
+        LightColorDetector newLightSource = new LightColorDetector();
+        String expectedRed = "Red";
+            String actualRed = newLightSource.detect(650);
+        checkColor(expectedRed, actualRed, "testColorDetectorShouldReturnRed");
+    }
+    public void checkColor(String expectedColor, String actualColor, String testName) {
+        if (expectedColor.equals(actualColor)) {
             System.out.println(testName + " has passed!");
         } else {
             System.out.println(testName + " has failed!");
-            System.out.println("Expected " + expectedInvisibleLight + " but was " + actualInvisibleLight);
-        }
+            System.out.println("Expected " + expectedColor + " but was " + actualColor);
+                    }
     }
-    public void checkViolet(String expectedViolet, String actualViolet, String testName) {
-        if (expectedViolet.equals(actualViolet)) {
-            System.out.println(testName + " has passed!");
-        } else {
-            System.out.println(testName + " has failed!");
-            System.out.println("Expected " + expectedViolet + " but was " + actualViolet);
-        }
-    }
-    public void checkBlue(String expectedBlue, String actualBlue, String testName) {
-        if (expectedBlue.equals(actualBlue)) {
-            System.out.println(testName + " has passed!");
-        } else {
-            System.out.println(testName + " has failed!");
-            System.out.println("Expected " + expectedBlue + " but was " + actualBlue);
-        }
-    }
-    public void checkGreen(String expectedGreen, String actualGreen, String testName) {
-        if (expectedGreen.equals(actualGreen)) {
-            System.out.println(testName + " has passed!");
-        } else {
-            System.out.println(testName + " has failed!");
-            System.out.println("Expected " + expectedGreen + " but was " + actualGreen);
-        }
-    }
-    public void checkYellow(String expectedYellow, String actualYellow, String testName) {
-        if (expectedYellow.equals(actualYellow)) {
-            System.out.println(testName + " has passed!");
-        } else {
-            System.out.println(testName + " has failed!");
-            System.out.println("Expected " + expectedYellow + " but was " + actualYellow);
-        }
-    }
-    public void checkOrange(String expectedOrange, String actualOrange, String testName) {
-        if (expectedOrange.equals(actualOrange)) {
-            System.out.println(testName + " has passed!");
-        } else {
-            System.out.println(testName + " has failed!");
-            System.out.println("Expected " + expectedOrange + " but was " + actualOrange);
-        }
-    }
-    public void checkRed(String expectedRed, String actualRed, String testName) {
-        if (expectedRed.equals(actualRed)) {
-            System.out.println(testName + " has passed!");
-        } else {
-            System.out.println(testName + " has failed!");
-            System.out.println("Expected " + expectedRed + " but was " + actualRed);
-        }
-    }
+
+
 
     public static void main(String[] args) {
         LightColorDetectorTest testColorDetector = new LightColorDetectorTest();
-        testColorDetector.testColorDetector();
+        testColorDetector.testColorDetectorShouldReturnInvisibleLight();
+        testColorDetector.testColorDetectorShouldReturnViolet();
+        testColorDetector.testColorDetectorShouldReturnBlue();
+        testColorDetector.testColorDetectorShouldReturnGreen();
+        testColorDetector.testColorDetectorShouldReturnYellow();
+        testColorDetector.testColorDetectorShouldReturnOrange();
+        testColorDetector.testColorDetectorShouldReturnRed();
 
     }
 }
