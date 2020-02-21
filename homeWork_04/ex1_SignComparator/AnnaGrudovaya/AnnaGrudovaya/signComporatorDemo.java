901fff6e164e33a142162142fd8becccf5fc5aa5
Anka330 <61016275+Anka330@users.noreply.github.com>
2020-02-22 02:06:03 +0300

    Add files via upload

---
+++ homeWork_04/ex1_SignComparator/AnnaGrudovaya/AnnaGrudovaya/signComporatorDemo.java
@@ -0,0 +1,12 @@
+package signComparator;
+
+public class signComporatorDemo {
+
+    public static void main(String[] args) {
+        signComparator comparator = new signComparator();
+        System.out.println("comparator.compare(0)  " + comparator.compare(0));
+        System.out.println("comparator.compare(10)  " + comparator.compare(10));
+        System.out.println("comparator.compare(-6)  " + comparator.compare(-6));
+
+    }
+}
