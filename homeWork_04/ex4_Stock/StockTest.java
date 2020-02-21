a688387dd609069e87cb822b1d8fa80d233487fd
vov4eg <a.vov4eg@gmail.com>
2020-02-21 21:14:18 +0300

    pls chk my hwk)) updated

--- homeWork_04/ex4_Stock/StockTest.java
+++ homeWork_04/ex4_Stock/StockTest.java
@@ -3,7 +3,6 @@ package ex4_Stock;
 public class StockTest {
     public static void main(String[] args) {
         Stock google = new Stock("Google", 100);
-        google.printInformation();
         google.updatePrice(1100);
         google.updatePrice(40);
         google.updatePrice(110);
