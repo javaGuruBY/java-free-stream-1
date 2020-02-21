4f947df1495071bb3cfbdc3c61f35cbb72e1ceb2
vov4eg <a.vov4eg@gmail.com>
2020-02-21 21:02:16 +0300

    pls chk my hwk)) updated

---
+++ homeWork_04/ex4_Stock/Stock.java
@@ -0,0 +1,32 @@
+package ex4_Stock;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Stock {
+    String name;
+    double actualPrice;
+    double minPrice;
+    double maxPrice;
+
+    public Stock(String name, double actualPrice) {
+        this.name = name;
+        this.actualPrice = actualPrice;
+        this.minPrice = this.actualPrice;
+        this.maxPrice = this.actualPrice;
+    }
+
+    public void updatePrice(double input) {
+        this.actualPrice = input;
+        if (this.actualPrice > this.maxPrice) {
+            this.maxPrice = this.actualPrice;
+        } else if (this.actualPrice < this.minPrice) {
+            this.minPrice = this.actualPrice;
+        }
+    }
+
+    public void printInformation() {
+        System.out.print("Company " + this.name + ". Actual price: " + this.actualPrice);
+        System.out.println(". Min / Max Price is: " + this.minPrice + " / " + this.maxPrice);
+        }
+}
---
+++ homeWork_04/ex4_Stock/StockTest.java
@@ -0,0 +1,13 @@
+package ex4_Stock;
+
+public class StockTest {
+    public static void main(String[] args) {
+        Stock google = new Stock("Google", 100);
+        google.printInformation();
+        google.updatePrice(1100);
+        google.updatePrice(40);
+        google.updatePrice(110);
+        google.updatePrice(50);
+        google.printInformation();
+    }
+}
