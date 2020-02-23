a105b31a6a16ca1ab65207bf190435556519270c
vov4eg <a.vov4eg@gmail.com>
2020-02-23 09:10:59 +0300

    pls chk my hwk)) updated

--- homeWork_04/ex1_SignComporator/SignComporatorTest.java
+++ homeWork_04/ex1_SignComporator/SignComporatorTest.java
@@ -3,41 +3,46 @@ package ex1_SignComporator;
 import java.util.Scanner;
 
 public class SignComporatorTest {
-    SignComporator check = new SignComporator();
+    SignComporator signComporator = new SignComporator();
 
     public static void main(String[] args) {
-        SignComporator check = new SignComporator();
-        SignComporatorTest test = new SignComporatorTest();
+        SignComporator signComporator = new SignComporator();
+        SignComporatorTest signComporatorTest = new SignComporatorTest();
         Scanner input = new Scanner(System.in);
         System.out.print("Введите число: ");
         int number = input.nextInt();
-        System.out.println(check.compare(number));
-        test.selfTestPositiveNumber();
-        test.selfTestNegativeNumber();
-        test.selfTestZero();
+        System.out.println(signComporator.compare(number));
+        signComporatorTest.testZero();
+        signComporatorTest.testNegative();
+        signComporatorTest.testPositive();
     }
 
-    public void selfTestPositiveNumber() {
-        if (check.compare(5) == check.positive) {
-            System.out.println("Positive self-test passed");
+   public void selfTestCode (String expectedResult, String actualResult, String description) {
+        if (actualResult.equals(expectedResult)) {
+            System.out.println(description + "passed");
         } else {
-            System.out.println("Self-test failed!");
+            System.out.println(description + "failed");
         }
-    }
+   }
 
-    public void selfTestNegativeNumber() {
-        if (check.compare(-5) == check.negative) {
-            System.out.println("Negative self-test passed");
-        } else {
-            System.out.println("Self-test failed!");
-        }
-    }
+   public void testZero() {
+        String expectedResult = signComporator.zero;
+        String acutalResult = signComporator.compare(0);
+        String description = "Test for 0 is ";
+        selfTestCode(expectedResult, acutalResult, description);
+   }
 
-    public void selfTestZero() {
-        if (check.compare(0) == check.zero) {
-            System.out.println("Zero self-test passed");
-        } else {
-            System.out.println("Self-test failed!");
-        }
-    }
+   public void testNegative() {
+        String expectedResult = signComporator.negative;
+        String actualResult = signComporator.compare(-1);
+        String description = "Test for negative number is ";
+        selfTestCode(expectedResult, actualResult, description);
+   }
+
+   public void testPositive() {
+        String expectedResult = signComporator.positive;
+        String actualResult = signComporator.compare(1);
+        String description = "Test for positive number is ";
+        selfTestCode(expectedResult, actualResult, description);
+   }
 }
