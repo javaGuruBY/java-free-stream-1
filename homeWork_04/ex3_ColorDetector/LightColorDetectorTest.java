70c98f94a0a3670b8e9de515014c7d39ac7dba4c
vov4eg <a.vov4eg@gmail.com>
2020-02-23 09:34:42 +0300

    pls chk my hwk)) updated

--- homeWork_04/ex3_ColorDetector/LightColorDetectorTest.java
+++ homeWork_04/ex3_ColorDetector/LightColorDetectorTest.java
@@ -1,20 +1,59 @@
 package ex3_ColorDetector;
 
-import java.sql.SQLOutput;
-
 public class LightColorDetectorTest {
+    LightColorDetector light = new LightColorDetector();
+
     public void testItSelf() {
-        LightColorDetector checkActualResult = new LightColorDetector();
+        testItSelfViolet();
+        testItSelfBlue();
+        testItSelfGreen();
+        testItSelfYellow();
+        testItSelfRed();
+        testItSelfInvisible();
+
+    }
+
+    public void testItSelfViolet() {
         String expectedResult = "Violet";
-        String actualResult = checkActualResult.detect(400);
+        String actualResult = light.detect(380);
+        checkMe(expectedResult, actualResult);
+    }
+
+    public void testItSelfBlue() {
+        String expectedResult = "Blue";
+        String actualResult = light.detect(450);
+        checkMe(expectedResult, actualResult);
+    }
+
+    public void testItSelfGreen() {
+        String expectedResult = "Green";
+        String actualResult = light.detect(495);
+        checkMe(expectedResult, actualResult);
+    }
+
+    public void testItSelfYellow() {
+        String expectedResult = "Yellow";
+        String actualResult = light.detect(570);
+        checkMe(expectedResult, actualResult);
+    }
+
+    public void testItSelfRed() {
+        String expectedResult = "Red";
+        String actualResult = light.detect(590);
+        checkMe(expectedResult, actualResult);
+    }
+
+    public void testItSelfInvisible() {
+        String expectedResult = "Invisible light";
+        String actualResult = light.detect(666);
         checkMe(expectedResult, actualResult);
     }
 
     public void checkMe(String expectedResult, String actualResult) {
-        if (expectedResult == actualResult) {
-            System.out.println("Self-test passed");
+        if (actualResult.equals(expectedResult)) {
+            System.out.println("Self-test for " + expectedResult + " passed");
         } else {
-            System.out.println("Self-test failed!");
+            System.out.println("Self-test for " + expectedResult + "FAILED!");
         }
     }
 }
